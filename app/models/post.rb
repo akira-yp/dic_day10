1059d407389e034c71bb890cc9c921b27f82eac0
Kaori Toyonaga <kaori_toyonaga@yahoo.co.jp>
2021-07-24 06:17:27 +0000

    add Validation to post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+  with_options presence: true do
+    validates :image 
+    validates :title, length: { in: 1..15 }
+    validates :content, length: { in: 1..250 }
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_24_053053) do
+ActiveRecord::Schema.define(version: 2021_07_24_053914) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "title", null: false
+    t.text "content", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "image"
     t.string "title"
     t.text "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-  
-  create_table "contacts", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "title", null: false
-    t.text "content", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
